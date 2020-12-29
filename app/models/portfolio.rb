5a589dae31337835687c41315929bd51a99b79e2
Jess <jess@Jess-Personal-MBP.local>
2020-12-28 23:24:22 -0800

    implemented concerns for placeholder

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image, message: "can't be blank"
 
   def self.angular
@@ -13,7 +14,8 @@ class Portfolio < ApplicationRecord
 
   # the pipe means only set the value if the variable at the left side of the equal sign is nil.
   def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200"
+
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,12 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :percent_utilized
+
+  after_initialize :set_defaults
+
+  # the pipe means only set the value if the variable at the left side of the equal sign is nil.
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+                  
+  end
 end
