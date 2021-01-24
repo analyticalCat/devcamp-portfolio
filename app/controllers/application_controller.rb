dbecd80742232580d640eefa9344c7ebd9157303
Jess <jess@Jess-Personal-MBP.local>
2021-01-23 22:41:15 -0800

    added changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,7 @@ class ApplicationController < ActionController::Base
 
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
 
-  def current_user #in Ruby you can override or overload the method.
-    #if current_user is not nil, do nothing
-    #else do the OpenStruct thing
-    super || OpenStruct.new(name: "Guest Hus", first_name: "Guest", last_nmae: "Hus", email: "guest@dfs.com")
 
-  end
 end
