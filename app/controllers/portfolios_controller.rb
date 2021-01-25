57d67d58d8d5ab9d4887206d10c61aaa547de68d
Jess <jess@Jess-Personal-MBP.local>
2021-01-24 23:13:53 -0800

    added layout files and styles

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_port, only:[:show, :edit, :update, :destroy]
+  layout "portfolio"
+
   def index
     @portfolio_items = Portfolio.all
     # @portfolio_items = Portfolio.where(subtitle: "Ruby on Rails")
